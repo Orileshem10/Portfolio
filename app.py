4aeecffdcd081ce2f8fd2c1062e6e8a381515162
Orileshem10 <orileshem10@gmail.com>
2025-10-10 15:33:16 -0400

    Refactor home route to only accept GET requests and remove debug mode from app.run()

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template , url_for, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
